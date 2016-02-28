06a302fc63441c61ec4bf51fd26b74631a863ec2
Hakim Hauston <jibostore@gmail.com>
2016-02-28 15:54:03 +0800

    mongoid: doesn't fill all fields on result

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,10 +6,34 @@ end
 
 class UserController < ApplicationController
   def fblogin
-    @userparam = User.new :fbid => params[:fbid], :facebookname => params[:facebookname], :competitorFbid => params[:competitorFbid]
-    @pojo = Pojo.new
-    @pojo.fbid = "566020605"
-    # render :json => @pojo #ok
-    render :json => @userparam.writeasjson
+    #@userparam = User.new :fbid => params[:fbid], :facebookname => params[:facebookname], :competitorFbid => params[:competitorFbid]
+    ##@pojo = Pojo.new
+    ##@pojo.fbid = "566020605"
+    ## render :json => @pojo #ok
+    #render :json => @userparam.writeasjson
+    #userExist = User.where(fbid: params[:fbid]).all.to_a
+    #render :json => userExist[0].writeasjson
+    
+=begin
+      userExistCriteria = User.where(fbid: params[:fbid])
+      userMe = userExistCriteria.each do | me |
+        userHash = {
+          :fbid => me.fbid, 
+          :facebookname => me.facebookname, 
+          :competitorFbid => me.competitorFbid, 
+          :createdAt => me.createdAt,
+          :updatedAt => me.updatedAt
+        }
+        #render :json => userHash  
+      end
+      userId = userExistCriteria.first
+      userCriteria = User.find(userId.id)
+      render :json => userCriteria
+=end
+    
+    
+    userSearchCriteria = User.find_by(fbid: params[:fbid])
+    render :json => userSearchCriteria      
+    
   end
 end
