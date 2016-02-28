7e0af1911ec55d422e32253b2114c8fb643aeaa3
Hakim Hauston <jibostore@gmail.com>
2016-02-28 12:57:28 +0800

    user: json response

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,15 @@
+class Pojo
+  @fbid
+  
+  attr_accessor :fbid
+end
+
 class UserController < ApplicationController
   def fblogin
     @userparam = User.new :fbid => params[:fbid], :facebookname => params[:facebookname], :competitorFbid => params[:competitorFbid]
+    @pojo = Pojo.new
+    @pojo.fbid = "566020605"
+    # render :json => @pojo #ok
+    render :json => @userparam.writeasjson
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+require 'pp'
+require 'json'
+
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  #field :_id, type: String, default: ->{ name } # which was `key :name` before v3
   field :fbid, type: String
   field :facebookname, type: String
   field :competitorFbid, type: String
@@ -12,6 +16,18 @@ class User
     @fbid = options[:fbid]
     @facebookname = options[:facebookname]
     @competitorFbid = options[:competitorFbid]
+  end
+  
+  def writeasjson
+    #JSON.parse(@self.to_json(:everything))
+    #@fbid
+    hash = { :fbid => @fbid, 
+      :facebookname => @facebookname, 
+      :competitorFbid => @competitorFbid, 
+      :createdAt => @createdAt,
+      :updatedAt => @updatedAt
+    }
+    hash.to_json
   end
   
   attr_accessor :fbid, :facebookname, :competitorFbid, :createdAt, :updatedAt
